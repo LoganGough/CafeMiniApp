31b94200efc9b5dc1cf33a39b1f63418f2a150bd
Logan <logan.gough26@d155.org>
2024-09-11 07:36:10 -0500

    Initial Commit

---
+++ CafeMiniApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CafeMiniApp
+//
+//  Created by LOGAN GOUGH on 9/11/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
